edb34dd37f85994a69ae48b287fd84961c3aa354
Justin021-pm <mrkz07eliel@gmail.com>
2025-02-07 22:52:16 -0600

    Avance ejercicio 2

--- Ejercicio 1.dart
+++ Ejercicio 1.dart
@@ -1,18 +1,15 @@
-//1. Cree un programa el cual imprima una lista con los primeros 15 números de la sucesión de Fibonacci
+ //1. Cree un programa el cual imprima una lista con los primeros 15 números de la sucesión de Fibonacci
 void main(){
-int n1=0;
-int n2=1;
-int ac=0;
+int n1 = 0;
+int n2 = 1;
+int ac = 0;
 print(n1);
 print(n2);
 
-for(int i=0; i<=13;i++){
-  ac=n1+n2;
+for(int i=0; i <= 13; i++){
+  ac = n1 + n2;
   print(ac);
-  n1=n2;
-  n2=ac;
-   
-
-  
+  n1 = n2;
+  n2 = ac;  
   }
 }
---
+++ Ejercicio2.dart
@@ -0,0 +1,27 @@
+//2. Crea un programa que devuelva una lista con todos los elementos únicos de otra lista.
+
+/*void main () {
+ final List<int> numeros = [1,2,3,4,5,6,7,5,2,4];
+ print(numeros);
+ /// print(elementos_unicos);
+}
+*/
+
+List<int> lista_unica<int> (final List<int> lista) {
+  final List<int> lista_duplicada = []; // lista para ingresar cada elemento unico de la lista original
+  for (int elemento in lista) { // para cada elemento en lista
+    if (!lista_duplicada.contains(elemento)) { // si lista duplicada no contiene al elemento de la lista original
+        lista_duplicada.add(elemento); // se agrega ese elemento a la nueva lista
+    }
+  }
+  return lista_duplicada;
+}
+
+void main() {
+  List<int> lista_int = [1, 2, 2, 3, 4, 4, 5, 6, 6, 7];
+  List<int> elementos_unicos = lista_unica(lista_int);
+  print("Lista original: $lista_int");
+  print("Lista con elementos unicos: $elementos_unicos");
+}
+
+
