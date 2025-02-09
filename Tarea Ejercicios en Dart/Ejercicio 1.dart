bb0b88736001f617cc8434c069ca8986806e6c03
Eddie <eddiemerino2704@gmail.com>
2025-02-08 19:59:55 -0600

    Cambio en el ejercicio1

--- Tarea Ejercicios en Dart/Ejercicio 1.dart
+++ Tarea Ejercicios en Dart/Ejercicio 1.dart
@@ -1,15 +1,20 @@
  //1. Cree un programa el cual imprima una lista con los primeros 15 números de la sucesión de Fibonacci
 void main(){
+//Lista donde se guardaran los primeros 15 numeros de la sucesion Fibonacci
+List <int> fibonacci=[];
 int n1 = 0;
 int n2 = 1;
 int ac = 0;
-print(n1);
-print(n2);
+fibonacci.add(n1);
+fibonacci.add(n2);
 
-for(int i=0; i <= 13; i++){
+for(int i=0; i <=12; i++){
   ac = n1 + n2;
-  print(ac);
+  fibonacci.add(ac);
   n1 = n2;
   n2 = ac;  
-  }
+}
+
+//Imprimir la sucesion de Fibonacci
+print(fibonacci);
 }
