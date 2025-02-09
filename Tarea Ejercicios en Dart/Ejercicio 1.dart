fffbde2841d159649ff65c9d294187c1cd4e4810
Eddie <eddiemerino2704@gmail.com>
2025-02-08 20:13:43 -0600

    Actualizacion ejercicio1

--- Tarea Ejercicios en Dart/Ejercicio 1.dart
+++ Tarea Ejercicios en Dart/Ejercicio 1.dart
@@ -2,17 +2,17 @@
 void main(){
 //Lista donde se guardaran los primeros 15 numeros de la sucesion Fibonacci
 List <int> fibonacci=[];
-int n1 = 0;
-int n2 = 1;
+int primero = 0;
+int segundo = 1;
 int ac = 0;
-fibonacci.add(n1);
-fibonacci.add(n2);
+fibonacci.add(primero);
+fibonacci.add(segundo);
 
 for(int i=0; i <=12; i++){
-  ac = n1 + n2;
+  ac = primero + segundo;
   fibonacci.add(ac);
-  n1 = n2;
-  n2 = ac;  
+  primero = segundo;
+  segundo = ac;  
 }
 
 //Imprimir la sucesion de Fibonacci
